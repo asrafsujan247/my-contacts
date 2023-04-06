436121dee90474cdb1c063011b9a7514db08ed46
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-06 12:20:50 +0600

    added edit compo and edit feature in the app

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./index.css";
 import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
