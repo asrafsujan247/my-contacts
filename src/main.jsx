88e4cbe2c197b2efae3b5a8b7367480f2a5ea273
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-06 11:48:41 +0600

    added root comp ad contacts compo and and the error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Root, { loader as rootLoader } from "./routes/root";
 import "./index.css";
+import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./routes/contact";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
