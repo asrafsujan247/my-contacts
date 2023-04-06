d6dafa4bc4adefd0b21aab04f1c11e74a8bee781
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-06 13:23:45 +0600

    added delete feature

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
